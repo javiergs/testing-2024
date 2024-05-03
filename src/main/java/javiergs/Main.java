32524ed471982741e2bece1f077cf7b8ea6220a3
Javier Gonzalez <3814755+javiergs@users.noreply.github.com>
2024-05-02 23:20:56 -0700

    Update Main.java

--- src/main/java/javiergs/Main.java
+++ src/main/java/javiergs/Main.java
@@ -2,7 +2,7 @@ package javiergs;
 
 public class Main {
   public static void main(String args[]) {
-    hypothenuse(10, 20);
+    System.out.println (hypothenuse(10, 20));
   }
   public static double hypothenuse(double a, double b) {
     return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
